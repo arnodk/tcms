2237a97e6ea6de3cd246ea656e3145e242d9af78
arno <arno@pz.nl>
2019-07-31 11:51:25 +0200

    added login unit test class

---
+++ code/tcms/tests/LoginTest.php
@@ -0,0 +1,24 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use tcms\Context;
+use tcms\Login;
+use tcms\Startup;
+
+class LoginTest extends TestCase
+{
+    public function __construct(?string $name = null, array $data = [], string $dataName = '')
+    {
+        require_once("../Startup.class.php");
+
+        Startup::load();
+
+        parent::__construct($name, $data, $dataName);
+    }
+
+    public function testLogin() {
+        $context = new Context();
+        $login = new Login($context);
+        $this->assertEquals(true,$login->loadForUser('root'), 'expected root user, but no root user found');
+    }
+}
